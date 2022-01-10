b4747ea15313a6c97277ade7415bbb5a1fa38d42
Hasnae Abouhilal <hasnae_abouhilal@hotmail.com>
2022-01-09 21:45:09 -0500

    addin geojason

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,35 +1,54 @@
-
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([33.9416, -118.4085], 5);
+// Add console.log to check to see if our code is working
+console.log('working');
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 4,
-    id: 'mapbox/streets-v11',
+let Streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
     accessToken: API_KEY
 });
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
 
-let JFK = [40.6417, -73.7809]
-let SFO = [37.6213, -122.3790]
-let AUS = [30.1974, -97.6663]
-let YYZ = [43.6775, -79.6308]
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 
-// Coordinates for each point to be used in the line.
-let line = [
-    SFO,
-    AUS,
-    YYZ,
-    JFK
-  ];
+// Create a base layer that holds both maps
+let baseMaps = {
+  'Streets': Streets,
+  'Satellite Streets': satelliteStreets
+};
 
-// Create a polyline using the line coordinates and make the line red
-L.polyline(line, {
-    color: 'blue',
-    width: 4,
-    opacity: .5,
-    dashArray: 4
-}).addTo(map);
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [Streets]
+  });
 
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+//Accessing Toronto neighborhoods GeoJSON URL
+let torontoHoods = "https://raw.githubusercontent.com/habouhilal/M13-Module_13_Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+// Create a style for the lines.
+let myStyle = {
+  color: "blue",
+  fillColor: "yellow",
+  weight: 1
+};
+
+// Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+    style: myStyle,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h2>" + feature.properties.AREA_NAME + "</h2>");
+    }
+  }).addTo(map);
+});
